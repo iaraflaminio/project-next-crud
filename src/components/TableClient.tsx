ca1e00a86af25c13e4a521f40f954ff5a9fec942
Iara Esteves Flaminio <iaraflaminio@outlook.com>
2024-07-23 13:38:58 -0300

    Implementing render functions for Header and Data

--- src/components/TableClient.tsx
+++ src/components/TableClient.tsx
@@ -1,17 +1,41 @@
 import Client from "@/core/Client"
 
-interface TableProps{
+interface TableProps {
     clients: Client[]
 }
 
-export default function TableClient(props: TableProps){
-    return (
-        <table>
+export default function TableClient(props: TableProps) {
+
+    function renderHeader() {
+        return (
             <tr>
                 <th>ID</th>
                 <th>Name</th>
                 <th>Age</th>
             </tr>
+        )
+    }
+
+    function renderData() {
+        return props.clients?.map((client, i) => {
+            return (
+                <tr key={client.id}>
+                    <td>{client.id}</td>
+                    <td>{client.name}</td>
+                    <td>{client.age}</td>
+                </tr>
+            )
+        })
+    }
+
+    return (
+        <table>
+            <thead>
+                {renderHeader()}
+            </thead>
+            <tbody>
+                {renderData()}
+            </tbody>
         </table>
     )
 }
