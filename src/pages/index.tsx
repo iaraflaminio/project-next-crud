91f31bf3365b0192d400a369dd7fb9165db558e1
Iara Esteves Flaminio <iaraflaminio@outlook.com>
2024-07-22 23:28:57 -0300

    update Home page with new Layout and Title components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,18 @@
+import Layout from "@/components/Layout";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main
-      className={`flex h-screen justify-center items-center bg-gradient-to-r from-purple-500 to-blue-600 ${inter.className}`}>
-        <span className="text-4xl">Testing</span>
-    </main>
+    <div
+      className={`flex h-screen justify-center items-center 
+      bg-gradient-to-r from-purple-500 to-blue-600 
+     ${inter.className}`}>
+      
+      <Layout Title="Simple CRUD">
+        <span>Content</span>
+      </Layout>
+    </div>
   );
 }
