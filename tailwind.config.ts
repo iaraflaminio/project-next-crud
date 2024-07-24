25aabae08f6c88b199e0d2221a536acffa16fae7
Iara Esteves Flaminio <iaraflaminio@outlook.com>
2024-07-24 18:44:28 -0300

    Adding safelist pattern for build

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,11 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  safelist: [
+    {
+      pattern: /(from|to)-(green|blue|gray)-(400|700)/,
+    }
+  ],
   theme: {
     extend: {
       backgroundImage: {
