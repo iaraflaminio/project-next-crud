de27baffe8540cbba35b037b794370b82a76f3b8
Iara Esteves Flaminio <iaraflaminio@outlook.com>
2024-07-28 00:30:55 -0300

    Reorganizing with new hook for table and form

--- src/hooks/useClients.ts
+++ src/hooks/useClients.ts
@@ -2,17 +2,19 @@ import ClientCollection from "@/backend/database/ClientCollection"
 import Client from "@/core/Client"
 import ClientRepository from "@/core/ClientRepository"
 import { useEffect, useState } from "react"
+import useTableOrForm from "./useTableForm"
 
 export default function useClients() {
-    const repo: ClientRepository = new ClientCollection()
+  const repo: ClientRepository = new ClientCollection()
+
+  const {tableVisible, formVisible, showTable, showForm} = useTableOrForm()
 
   const [client, setClient] = useState<Client>(Client.empty())
   const [clients, setClients] = useState<Client[]>([])
-  const [visibleTable, setVisible] = useState<'table' | 'form'>('table')
 
   useEffect(getAllClients, [])
 
-  function getAllClients(){
+  function getAllClients() {
     repo.getAll().then(clients => {
       setClients(clients)
       setVisible('table')
@@ -29,7 +31,7 @@ export default function useClients() {
     getAllClients()
   }
 
-  function newClient(){
+  function newClient() {
     setClient(Client.empty())
     setVisible('form')
   }
@@ -43,7 +45,7 @@ export default function useClients() {
     client,
     clients,
     newClient,
-    saveClient, 
+    saveClient,
     deleteClient,
     selectClient,
     getAllClients,
